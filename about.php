3e7ebfa31fb64c6627a4de524315a60c0cc59e23
Aleksa Jovanovic <aleksa.jovanovic.97@gmail.com>
2021-05-04 10:50:35 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,135 @@
+<?php include("head.php"); ?>
+
+<style>
+body{background:#fff;}
+.banner2{width:100%; height:550px; background:url(slike/aboutimg.jpg); background-size:cover; background-position:top; background-repeat:no-repeat;}
+.banner2 h1{width:1000px; min-height:100px; line-height:100px; font-size:80px; font-family:geomanist; color:#fff; letter-spacing:2px; margin:0 auto; text-align:center; padding-top:180px;}
+
+.nosiostalo{width:1300px; min-height:500px; margin:20px auto 50px auto;}
+.nosiostalo::after{clear:both; display:table; content:"";}
+.levo{float:left; width:55%; height:505px; margin:10px 0;}
+.levo::after{clear:both; display:table; content:"";}
+.levo img{display:block; float:left; width:270px; height:360px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);}
+.prva{margin:40px 0 0 60px;}
+.druga{margin:140px 0 0 50px;}
+
+.desno{float:left; width:45%; min-height:480px; margin:10px 0;}
+.desno::after{clear:both; display:table; content:"";}
+.desno div{width:100%; min-height:300px; font-size:32px; line-height:46px; font-family:geomanist; color:#333333; white-space:pre-wrap; padding:0 0 0 15px;}
+.desno p{width:100%; min-height:180px; margin:0; padding:2px 10px 10px 15px; white-space:pre-wrap; font-size:22px; line-height:26px; font-family:geomanist; color:#555; font-weight:lighter;}
+.traka{width:1200px; height:2px; background:#e8e8e8; margin:auto;}
+
+.nosidrugo{width:1210px; min-height:200px; margin:30px auto;}
+.nosidrugo::after{clear:both; display:table; content:"";}
+.drugo1{width:380px; height:180px; margin:10px 10px; float:left;}
+.drugo1 header{width:380px; height:70px;}
+.drugo1 header::after{clear:both; display:table; content:"";}
+.drugo1 header img{display:block; float:left; width:40px; height:40px; margin:14px;}
+.drugo1 header div{width:260px; height:70px; line-height:18px; float:left; text-align:left; font-family:geomanist; padding-top:22px; font-weight:bold;}
+.drugo1 main{width:380px; height:110px; padding:10px; font-size:17px; line-height:30px; font-family:geomanist; color:#333333;}
+
+.drugo2{width:380px; height:180px; margin:10px 10px; float:left;}
+.drugo2 header{width:100%; height:70px;}
+.drugo2 header::after{clear:both; display:table; content:"";}
+.drugo2 header img{display:block; float:left; width:40px; height:40px; margin:14px;}
+.drugo2 header div{width:260px; height:70px; line-height:18px; float:left; text-align:left; font-family:geomanist; padding-top:27px; font-weight:bold;}
+.drugo2 main{width:380px; height:110px; padding:10px; font-size:17px; line-height:30px; font-family:geomanist; color:#333333;}
+
+.drugo3{width:380px; height:180px; margin:10px 10px; float:left;}
+.drugo3 header{width:100%; height:70px;}
+.drugo3 header::after{clear:both; display:table; content:"";}
+.drugo3 header img{display:block; float:left; width:40px; height:40px; margin:14px;}
+.drugo3 header div{width:260px; height:70px; line-height:18px; float:left; text-align:left; font-family:geomanist; padding-top:25px; font-weight:bold;}
+.drugo3 main{width:380px; height:110px; padding:10px; font-size:17px; line-height:30px; font-family:geomanist; color:#333333;}
+
+.zavrsnibaner{width:100%; height:500px; margin:30px auto; background:url(slike/bar.jpg); background-size:cover; background-repeat:no-repeat; background-position:center;}
+
+@media only screen and (max-width: 480px){
+.banner2{height:350px;}
+.banner2 h1{width:100%; font-size:40px; line-height:60px; padding-top:35%;}	
+.nosiostalo{width:100%; height:1870px; margin:40px auto;}
+.levo{width:100%; margin:10px 0; height:870px;}
+.levo img{width:80%; height:400px;}
+.desno{width:100%; height:1000px;}
+.desno div{text-align:center; padding-left:8px;}
+.prva{margin:5% 10%;}
+.druga{margin:5% 10%;}
+.traka{width:80%; margin:10%;}	
+.nosidrugo{width:100%;}
+}
+</style>
+
+<div class="banner2">
+<h1>ABOUT</h1>
+</div>
+
+<div class="nosiostalo">
+
+<div class="levo">
+<img class="prva" src="slike/about2.jpg">
+<img class="druga" src="slike/about1.jpg">
+</div>
+
+<div class="desno">
+<div>
+MODERN AND STYLISH BURGER
+BAR. IN OUR MENU YOU WILL FIND
+BOTH CLASSIC BURGERS AND
+UNUSUAL AND UNPREDICTABLE
+RECIPES FROM OUR CHIEF COOK.
+</div>
+<p>
+Sed rutrum, libero non pretium tristique, arcu mi sollicitudin ex,
+quis venenatis orci tellus vel dolor. Donec gravida, dolor ut
+auctor facilisis, enim dolor pellentesque lectus
+
+Sed ultricies lacus ut libero faucibus fringilla. Ut nisi tellus,
+posuere vel mattis nec, convallis a metus. Nullam elementum
+molestie felis nec lobortis. Cras at justo eu elit semper tempor
+sed quis orci. In risus magna.
+</p>
+</div>
+</div>
+
+<div class="traka"></div>
+
+<div class="nosidrugo">
+
+<div class="drugo1">
+<header>
+<img src="slike/about3.jpg">
+<div>HIGH QUALITY PRODUCTS</div>
+</header>
+<main>Drinking vinegar stumptown yr pop-up artisan
+sunt deep v cliche lomo biodiesel</main>
+</div>
+
+<div class="drugo2">
+<header>
+<img src="slike/about4.jpg">
+<div>COZY ATMOSPHERE</div>  
+</header>
+<main>
+Truffaut synth art party deep v chillwave Seitan
+High Life cupidatat kogi
+</main>
+</div>
+
+<div class="drugo3">
+<header>
+<img src="slike/about5.jpg">
+<div>REASONABLE PRICES</div>
+</header>
+<main>
+Drinking vinegar stumptown yr pop-up artisan
+sunt deep v cliche lomo biodiesel
+</main>
+</div>
+</div>
+
+<div class="zavrsnibaner">
+
+</div>
+
+
+<?php include("futer.php"); ?>
